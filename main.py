8d8e673050a6407648ba65113301e3b5c13173d2
itsmekamp <kawijaya137@gmail.com>
2018-06-26 01:53:30 +0700

    add py file

---
+++ main.py
@@ -0,0 +1,16 @@
+def genFib(n):
+   if n == 1:
+      return 1
+   elif n == 0:
+      return 0
+   else:
+      return genFib(n-1) + genFib(n-2)
+
+def Fibonacci(x):
+    for i in range(x):
+        print(genFib(i+1))
+
+print("Fibonacci Generator")
+uInput = int(input("How many numbers would you generate?\n"))
+if not uInput == 0:
+    Fibonacci(uInput)
